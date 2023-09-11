aa0a46c3bc72ed48b3e2c0d376953db0db0b81ce
michimichi2021 <michikaho1009@gmail.com>
2023-09-11 18:24:46 +0900

    認証部分の実装をauthモジュールに書き換えた

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,62 +1,90 @@
 <template>
   <div id="app" class="mx-auto mt-5">
-    <authenticator>
-      <template v-slot="{ user, signOut }">
-        <v-title> Hello {{ user.username }}! </v-title>
-        <v-btn v-on:click="signOut">Sign Out</v-btn>
-        <v-card class="mx-auto mt-10" max-width="1000">
-          <v-card-title class="text-center">Todo App</v-card-title>
-          <v-form>
-            <v-container>
-              <v-row>
-                <v-col cols="12" md="6">
-                  <v-text-field v-model="name" :counter="10" label="題名" required></v-text-field>
-                </v-col>
-                <v-col cols="12" md="6">
-                  <v-text-field
-                    v-model="description"
-                    :counter="10"
-                    label="内容"
-                    required
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-              <v-btn v-on:click="ClickCreate">Create Todo</v-btn>
-            </v-container>
-          </v-form>
-          <v-toolbar color="cyan-lighten-1">
-            <v-btn variant="text" icon="mdi-menu"></v-btn>
-            <v-spacer></v-spacer>
-            <v-btn variant="text" icon="mdi-magnify"></v-btn>
-          </v-toolbar>
-          <v-card-title class="text-center">実行中</v-card-title>
-          <v-list-item v-for="todo in todos" :key="todo.id">
-            <v-li class="list-group-item list-group-item-action" v-if="!todo.completed">
-              題名:{{ todo.name }}
-              <hr />
-              内容:{{ todo.description }}
-              <hr />
-              <v-btn v-if="!todo.completed" v-on:click="CompletedTodo(todo)">完了</v-btn>
-            </v-li>
-          </v-list-item>
-          <v-card-title class="text-center">完了済み</v-card-title>
-          <v-list-item v-for="todo in todos" :key="todo.id">
-            <v-li class="list-group-item list-group-item-action" v-if="todo.completed">
-              題名:{{ todo.name }}
-              <hr />
-              内容:{{ todo.description }}
-              <hr />
-              <v-btn v-on:click="ClickDelete(todo.id)">削除</v-btn>
-            </v-li>
-          </v-list-item>
-        </v-card>
-      </template>
-    </authenticator>
+    <div v-if="!user.username">
+      <div v-if="isSignUpMode">新規登録画面</div>
+      <div v-if="!isSignUpMode">ログイン画面</div>
+      <v-btn class="mr-2" color="success" v-on:click="toggleSignUpMode"
+        >切り替えボタン ログイン/新規登録</v-btn
+      >
+      <div class="mt-5">
+        <div v-if="isSignUpMode">
+          <input v-model="username" placeholder="Username" />
+          <input v-if="isSignUpMode" v-model="email" placeholder="Email" />
+          <input v-model="password" placeholder="Password" type="password" />
+          <v-btn class="mr-2" color="success" v-on:click="signUp(username, email, password)"
+            >新規登録</v-btn
+          >
+        </div>
+        <div v-if="!isSignUpMode">
+          <input v-model="username" placeholder="Username" />
+          <input v-model="password" placeholder="Password" type="password" />
+          <v-btn class="mr-2" color="success" v-on:click="signIn(username, password)"
+            >ログイン</v-btn
+          >
+        </div>
+        <div v-if="!isConfirmationMode && isSignUpMode">
+          <input v-model="confirmationCode" placeholder="Confirmation Code" />
+          <v-btn color="success" v-on:click="confirmSignUp(username, confirmationCode)"
+            >2段階認証する</v-btn
+          >
+        </div>
+      </div>
+    </div>
+    <div v-if="user.username">
+      <v-title> Hello {{ user.username }}! </v-title>
+      <v-btn v-on:click="signOut">Sign Out</v-btn>
+      <v-card class="mx-auto mt-10" max-width="1000">
+        <v-card-title class="text-center">Todo App</v-card-title>
+        <v-form>
+          <v-container>
+            <v-row>
+              <v-col cols="12" md="6">
+                <v-text-field v-model="name" :counter="10" label="題名" required></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  v-model="description"
+                  :counter="10"
+                  label="内容"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-btn v-on:click="ClickCreate">Create Todo</v-btn>
+          </v-container>
+        </v-form>
+        <v-toolbar color="cyan-lighten-1">
+          <v-btn variant="text" icon="mdi-menu"></v-btn>
+          <v-spacer></v-spacer>
+          <v-btn variant="text" icon="mdi-magnify"></v-btn>
+        </v-toolbar>
+        <v-card-title class="text-center">実行中</v-card-title>
+        <v-list-item v-for="todo in todos" :key="todo.id">
+          <li class="list-group-item list-group-item-action" v-if="!todo.completed">
+            題名:{{ todo.name }}
+            <hr />
+            内容:{{ todo.description }}
+            <hr />
+            <v-btn v-if="!todo.completed" v-on:click="CompletedTodo(todo)">完了</v-btn>
+          </li>
+        </v-list-item>
+        <v-card-title class="text-center">完了済み</v-card-title>
+        <v-list-item v-for="todo in todos" :key="todo.id">
+          <li class="list-group-item list-group-item-action" v-if="todo.completed">
+            題名:{{ todo.name }}
+            <hr />
+            内容:{{ todo.description }}
+            <hr />
+            <v-btn v-on:click="ClickDelete(todo.id)">削除</v-btn>
+          </li>
+        </v-list-item>
+      </v-card>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { API } from 'aws-amplify'
+import { API, Auth } from 'aws-amplify'
 import { reactive, ref, toRefs } from 'vue'
 import { createTodo, deleteTodo, updateTodo } from '../graphql/mutations'
 import type {
@@ -68,10 +96,71 @@ import type {
   UpdateTodoMutation,
   UpdateTodoInput
 } from '../API'
-import { Authenticator } from '@aws-amplify/ui-vue'
 import '@aws-amplify/ui-vue/styles.css'
 import { listTodos } from '../graphql/queries'
 import type { GraphQLQuery, GraphQLResult } from '@aws-amplify/api'
+
+const user = reactive({ username: null as string | null })
+const username = ref('')
+const password = ref('')
+const email = ref('')
+const isSignUpMode = ref(false)
+
+const toggleSignUpMode = () => {
+  isSignUpMode.value = !isSignUpMode.value
+}
+
+const confirmationCode = ref('')
+const isConfirmationMode = ref(true)
+
+const signUp = async (usernameParam: string, emailParam: string, passwordParam: string) => {
+  try {
+    const data = await Auth.signUp({
+      username: usernameParam,
+      password: passwordParam,
+      attributes: {
+        email: emailParam
+      }
+    })
+    console.log(data)
+    isConfirmationMode.value = !isConfirmationMode.value
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const confirmSignUp = async (usernameParam: string, confirmationCodeParam: string) => {
+  try {
+    await Auth.confirmSignUp(usernameParam, confirmationCodeParam)
+    isSignUpMode.value = !isSignUpMode.value
+    username.value = ''
+    email.value = ''
+    password.value = ''
+    confirmationCode.value = ''
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const signIn = async (usernameParam: string, passwordParam: string) => {
+  try {
+    await Auth.signIn(usernameParam, passwordParam)
+    user.username = usernameParam
+    username.value = ''
+    password.value = ''
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const signOut = async () => {
+  try {
+    await Auth.signOut()
+    user.username = null
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 const todo = reactive({
   name: '',
